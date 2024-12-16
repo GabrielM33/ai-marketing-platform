6d0bd02c732ca32cab5be21e38613f9ec504bd45
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-16 20:56:54 -0300

    Add Dockerfile for asset processing service and update main.py to print messages periodically

--- asset-processing-service/asset_processing_service/main.py
+++ asset-processing-service/asset_processing_service/main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello World")
+from time import sleep
 
+def main():
+    while True:
+        print("Hello World", flush=True)
+        sleep(5)
 
 if __name__ == "__main__":
     main()
