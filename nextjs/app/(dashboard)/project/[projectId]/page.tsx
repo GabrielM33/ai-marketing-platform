708e47fe9eeec2cc2b65df9810c101f56b8a0181
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-17 15:08:38 -0300

    Refactor ProjectPage component to resolve params asynchronously and improve type definitions
    - Updated the `params` type to be a Promise, ensuring proper resolution before usage.
    - Removed the `generateStaticParams` function as it is no longer needed.
    - Enhanced the notFound logic to work with resolved parameters for better error handling.

--- nextjs/app/(dashboard)/project/[projectId]/page.tsx
+++ nextjs/app/(dashboard)/project/[projectId]/page.tsx
@@ -1,24 +1,17 @@
 import { notFound } from "next/navigation";
 
 type Props = {
-  params: {
+  params: Promise<{
     projectId: string;
-  };
-  searchParams?: { [key: string]: string | string[] | undefined };
+  }>;
 };
 
-export async function generateStaticParams() {
-  const projects = ["123", "456", "789"];
-
-  return projects.map((projectId) => ({
-    projectId: projectId,
-  }));
-}
-
 export default async function ProjectPage({ params }: Props) {
-  if (params.projectId != "123") {
+  const resolvedParams = await params;
+
+  if (resolvedParams.projectId != "123") {
     return notFound();
   }
 
-  return <div>Project Page: {params.projectId}</div>;
+  return <div>Project Page: {resolvedParams.projectId}</div>;
 }
