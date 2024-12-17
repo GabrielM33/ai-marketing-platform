7805af5021af865fcc14a29035a7631a0301007d
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-17 19:11:09 -0300

    Add Clerk authentication support

--- nextjs/app/(dashboard)/layout.tsx
+++ nextjs/app/(dashboard)/layout.tsx
@@ -1,14 +1,14 @@
-// layout.tsx
-
-import { Sidebar, SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/Sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div>
-      <SidebarProvider>
-        <Sidebar />
+    <SidebarProvider>
+      <AppSidebar />
+      <main>
+        <SidebarTrigger />
         {children}
-      </SidebarProvider>
-    </div>
+      </main>
+    </SidebarProvider>
   );
 }
