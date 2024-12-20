2f7f7f0bce1cba18e5b7c5d2e7d78b48103db51e
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-20 09:40:29 -0300

    Implement editable project title functionality in ProjectDetailHeader
    - Added state management for project title editing, including handling title changes and submission.
    - Introduced UI elements for editing the project title, including an input field and action buttons for saving or canceling edits.
    - Enhanced the component's structure to conditionally render the title or the editing interface based on the editing state.
    
    These changes improve user interaction by allowing real-time editing of project titles, enhancing the overall functionality of the project detail view.

--- nextjs/components/project-detail/ProjectDetailHeader.tsx
+++ nextjs/components/project-detail/ProjectDetailHeader.tsx
@@ -1,9 +1,19 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "../ui/input";
+import { Turtle } from "lucide-react";
 function ProjectDetailHeader() {
   //TODO: create edit title functionality
+  const [title, setTitle] = useState("project.title");
+  const [isEditing, setIsEditing] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
 
   //TODO: useState for title and edited title
+  const handleTitleSubmit = () => {};
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  };
 
   //TODO: save title to db fucntion
 
@@ -12,7 +22,37 @@ function ProjectDetailHeader() {
   //TODO: create delete project functionality
 
   //TODO: create DELETE request endpoint for project deletion
+  const handleDelete = () => {};
 
-  return <div>ProjectDetailHeader</div>;
+  if (isEditing) {
+    return (
+      <div>
+        {/* Input */}
+        <Input
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+            console.log(e.target.value);
+          }}
+        />
+        {/* Action Buttons */}
+        <div>
+          <Button onClick={() => setIsEditing(false)}>Cancel</Button>
+          <Button onClick={() => setIsEditing(false)}>Save</Button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      {/* if editing, show input field and save button // if not editing, show title and edit button */}
+      <h1>{title}</h1>
+      <div>
+        <Button onClick={() => setIsEditing(true)}>Edit</Button>
+        <Button onClick={() => setIsDeleting(true)}>Delete</Button>
+      </div>
+    </div>
+  );
 }
 export default ProjectDetailHeader;
