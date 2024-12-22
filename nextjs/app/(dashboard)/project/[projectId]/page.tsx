34ae7e2bd513d62fb480e7085a7e038b14651fc6
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-22 08:09:07 -0300

    Refactor ProjectPage to streamline parameter handling and enhance type safety
    - Removed the searchParams type definition from ProjectPageProps, simplifying the component's props.
    - Updated the function to focus on fetching the project based on the projectId parameter, improving clarity and maintainability.
    - Enhanced error handling by ensuring the notFound() function is called if the project is not found.
    
    These changes improve the overall structure and robustness of the ProjectPage component, aligning it with best practices for asynchronous data fetching in Next.js.

--- nextjs/app/(dashboard)/project/[projectId]/page.tsx
+++ nextjs/app/(dashboard)/project/[projectId]/page.tsx
@@ -3,18 +3,14 @@ import { getProject } from "@/server/queries";
 import { notFound } from "next/dist/client/components/not-found";
 
 type Params = Promise<{ projectId: string }>;
-type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
 type ProjectPageProps = {
   params: Params;
-  searchParams: SearchParams;
 };
 
 export default async function ProjectPage(props: ProjectPageProps) {
   try {
     const params = await props.params;
-    const searchParams = await props.searchParams;
-
     const project = await getProject(params.projectId);
 
     if (!project) {
