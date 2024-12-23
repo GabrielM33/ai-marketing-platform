43d91d878182657ce2cbbae9c6eab7ea38f51f6e
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2024-12-23 15:22:41 -0300

    Refactor ProjectDetailBody to support dynamic step rendering and improve loading experience
    - Enhanced the ProjectDetailBody component to accept dynamic steps and the current step index as props, allowing for flexible rendering of project steps.
    - Implemented a Suspense fallback with a skeleton loader to improve user experience during component loading.
    - Added a ProjectStepHeader for better context within the project detail view.
    - Introduced a StepSkeleton component to provide visual feedback while the current step is being loaded.
    
    These changes improve the modularity and responsiveness of the project detail interface, facilitating a smoother user experience.

--- nextjs/components/project-detail/ProjectDetailBody.tsx
+++ nextjs/components/project-detail/ProjectDetailBody.tsx
@@ -1,6 +1,43 @@
 "use client";
 
-export default function ProjectDetailBody() {
-  // TODO: Look at the current tab and change the project step to show to our users
-  return <div>Project Detail Body</div>;
+import { Suspense } from "react";
+import { Skeleton } from "../ui/skeleton";
+import ProjectStepHeader from "../upload-step/UploadStepHeader";
+
+interface ProjectDetailBodyProps {
+  steps: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    component: React.LazyExoticComponent<React.ComponentType<any>>;
+  }[];
+  currentStep: number;
+  projectId: string;
 }
+
+function ProjectDetailBody({
+  steps,
+  currentStep,
+  projectId,
+}: ProjectDetailBodyProps) {
+  const CurrentStepComponent = steps[currentStep].component;
+
+  return (
+    <Suspense fallback={<StepSkeleton />}>
+      <ProjectStepHeader projectId={projectId} />
+      <CurrentStepComponent projectId={projectId} />
+    </Suspense>
+  );
+}
+
+export default ProjectDetailBody;
+
+const StepSkeleton = () => {
+  return (
+    <div>
+      <Skeleton className="h-4 w-24 mb-4" />
+      <Skeleton className="h-10 w-full mb-4" />
+      <Skeleton className="h-10 w-full mb-4" />
+      <Skeleton className="h-10 w-full mb-4" />
+      <Skeleton className="h-10 w-full mb-4" />
+    </div>
+  );
+};
