46fb40f4138cba8fbbef2a800c4ab324d161c269
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-06 11:07:18 -0300

    update projectlist

--- nextjs/components/ProjectList.tsx
+++ nextjs/components/ProjectList.tsx
@@ -1,47 +1,33 @@
+import { Project } from "@/server/db/schema";
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardHeader, CardTitle } from "./ui/card";
 import Link from "next/link";
-import { getTimeAgo } from "@/utils/timeUtils";
-
-export type Project = {
-  id: string;
-  title: string;
-  description: string;
-  user_id: string;
-  created_at: string;
-  updated_at: string;
-};
+import { getTimeDifference } from "@/utils/timeUtils";
 
 interface ProjectListProps {
   projects: Project[];
 }
 
-export default function ProjectList({ projects }: ProjectListProps) {
+function ProjectList({ projects }: ProjectListProps) {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {projects?.map((project) => (
+    <div className="grid gap-4 sm:gap-6 md:gap-8 lg:gap-10 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      {/* TODO: iterate through each project and convert it into its own card */}
+      {projects.map((project) => (
         <Link key={project.id} href={`/project/${project.id}`}>
-          <Card>
-            <CardHeader>
-              <CardTitle>{project.title}</CardTitle>
-              {project.description && (
-                <CardDescription>{project.description}</CardDescription>
-              )}
+          <Card className="border border-gray-200 rounded-3xl p-3 hover:border-main hover:scale-[1.01] hover:shadow-md hover:text-main transition-all duration-300">
+            <CardHeader className="pb-3 sm:pb-4 lg:pb-5 w-full">
+              <CardTitle className="text-lg sm:text-xl lg:text-2xl truncate">
+                {project.title}
+              </CardTitle>
+              <p className="text-xs sm:text-sm text-gray-500 truncate">
+                Updated {getTimeDifference(project.updatedAt)}
+              </p>
             </CardHeader>
-            <CardContent>
-              <div className="text-sm text-gray-500">
-                Last updated: {getTimeAgo(new Date(project.updated_at))}
-              </div>
-            </CardContent>
           </Card>
         </Link>
       ))}
     </div>
   );
 }
+
+export default ProjectList;
