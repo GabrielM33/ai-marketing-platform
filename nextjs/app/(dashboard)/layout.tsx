f2c6407a26850d2114a2777cdab25a6f791a2a2b
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-06 11:03:08 -0300

    Refactor dashboard layout for improved structure and styling
    - Updated the layout component to enhance the overall structure by wrapping the sidebar and main content in a flex container, ensuring a responsive design.
    - Introduced a sticky header for the SidebarTrigger, improving accessibility and user experience.
    - Adjusted padding and height settings for the main content area to provide a better visual hierarchy and usability.
    
    These changes streamline the dashboard layout, enhancing the user interface and interaction.

--- nextjs/app/(dashboard)/layout.tsx
+++ nextjs/app/(dashboard)/layout.tsx
@@ -4,11 +4,15 @@ import { AppSidebar } from "@/components/Sidebar";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <AppSidebar />
-      <main>
-        <SidebarTrigger />
-        {children}
-      </main>
+      <div className="flex min-h-screen w-full">
+        <AppSidebar />
+        <main className="flex-1 w-full">
+          <div className="sticky top-4 z-10 border-b w-full">
+            <SidebarTrigger />
+          </div>
+          <div className="h-full w-full p-6">{children}</div>
+        </main>
+      </div>
     </SidebarProvider>
   );
 }
