db349d08af04b671bac713c0d6b0d2ffb2153f07
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-06 11:05:50 -0300

    update layout

--- nextjs/app/(dashboard)/layout.tsx
+++ nextjs/app/(dashboard)/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
       <div className="flex min-h-screen w-full">
         <AppSidebar />
         <main className="flex-1 w-full">
-          <div className="sticky top-4 z-10 border-b w-full">
+          <div className="sticky z-10 border-b w-full bg-gray-50">
             <SidebarTrigger />
           </div>
           <div className="h-full w-full p-6">{children}</div>
