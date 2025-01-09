9b33c2b5aacc5e00d0b63084b5d71798542c4955
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-09 10:51:54 -0300

    define update_job_hearlbeat

--- asset-processing-service/asset_processing_service/api_client.py
+++ asset-processing-service/asset_processing_service/api_client.py
@@ -1,24 +1,19 @@
 from datetime import datetime
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 import aiohttp
 from asset_processing_service.config import HEADERS, config
-from asset_processing_service.models import AssetProcessingJob
+from asset_processing_service.models import Asset, AssetProcessingJob
 
-"""
-    Asynchronously fetches asset processing jobs from the API.
 
-    Makes a GET request to the asset-processing-jobs endpoint and converts
-    the JSON response into AssetProcessingJob objects.
-
-    Returns:
-        List[AssetProcessingJob]: List of jobs fetched from the API
-        Returns empty list if the request fails
-    """
+class ApiError(Exception):
+    def __init__(self, message: str, status_code: int = None):
+        self.message = message
+        self.status_code = status_code
+        super().__init__(self.message)
 
 
 async def fetch_jobs() -> List[AssetProcessingJob]:
-
     try:
         url = f"{config.API_BASE_URL}/asset-processing-job"
 
@@ -39,23 +34,7 @@ async def fetch_jobs() -> List[AssetProcessingJob]:
         return []
 
 
-"""
-    Updates the details of a specific job in the API.
-
-    Makes a PATCH request to update job status, adding the current timestamp
-    as the lastHeartbeat.
-
-    Args:
-        job_id: The ID of the job to update
-        update_data: Dictionary containing the fields to update (status, errorMessage, etc.)
-
-    Raises:
-        Prints error message if the update request fails
-    """
-
-
 async def update_job_details(job_id: str, update_data: Dict[str, Any]) -> None:
-
     data = {**update_data, "lastHeartBeat": datetime.now().isoformat()}
     try:
         url = f"{config.API_BASE_URL}/asset-processing-job?jobId={job_id}"
@@ -64,3 +43,47 @@ async def update_job_details(job_id: str, update_data: Dict[str, Any]) -> None:
                 response.raise_for_status()
     except aiohttp.ClientError as error:
         print(f"Failed to update job details for job {job_id}: {error}")
+
+
+async def update_job_heartbeat(job_id: str) -> None:
+    try:
+        url = f"{config.API_BASE_URL}/asset-processing-job?jobId={job_id}"
+        data = {"lastHeartBeat": datetime.now().isoformat()}
+        async with aiohttp.ClientSession() as session:
+            async with session.patch(url, json=data, headers=HEADERS) as response:
+                response.raise_for_status()
+    except aiohttp.ClientError as error:
+        print(f"Failed to update job heartbeat for job {job_id}: {error}")
+
+
+async def fetch_asset(asset_id: str) -> Optional[Asset]:
+    try:
+        url = f"{config.API_BASE_URL}/asset?assetId={asset_id}"
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=HEADERS) as response:
+                if response.status == 200:
+                    data = await response.json()
+
+                    if data:
+                        return Asset(**data)
+
+                    return None
+
+                else:
+                    print("Error fetching asset: ", response.status)
+                    return None
+    except aiohttp.ClientError as error:
+        print(f"Error fetching asset: {error}")
+        return None
+
+
+async def fetch_asset_file(file_url: str) -> bytes:
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(file_url, headers=HEADERS) as response:
+                response.raise_for_status()
+                return await response.read()
+    except aiohttp.ClientError as error:
+        print(f"Error fetching asset file: {error}")
+        raise ApiError("Failed to fetch asset file", status_code=500)
