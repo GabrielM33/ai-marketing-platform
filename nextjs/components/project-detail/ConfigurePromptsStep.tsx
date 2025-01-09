e95e52758891e4ec2aabd811838c00190246d715
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-09 16:19:52 -0300

    Refactor ConfigurePromptsStep component to include additional UI elements
    - Integrated ConfigurePromptsStepHeader, PromptList, ConfirmationModal, ConfirmationDialog, and ConfigurePromptsForm into the ConfigurePromptsStep component for enhanced functionality and user experience.

--- nextjs/components/project-detail/ConfigurePromptsStep.tsx
+++ nextjs/components/project-detail/ConfigurePromptsStep.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import ConfirmationModal from "./ConfirmationModal";
+import ConfigurePromptsStepHeader from "./ConfigurePromptsStepHeader";
+import PromptList from "./PromptList";
+import ConfirmationDialog from "../ConfirmationDialog";
+import ConfigurePromptsForm from "./ConfigurePromptsForm";
 
 function ConfigurePromptsStep() {
-  return <div>ConfigurePromptsStep</div>;
+  return (
+    <div>
+      <ConfigurePromptsStepHeader />
+      <PromptList />
+      <ConfirmationModal />
+      <ConfirmationDialog />
+      <ConfigurePromptsForm />
+    </div>
+  );
 }
 
 export default ConfigurePromptsStep;
