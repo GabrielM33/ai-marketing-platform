9b440e26be5e890e650801b562934e9b715d901f
Gabriel Moraes <gabrielmoraesweb@gmail.com>
2025-01-10 11:06:07 -0300

    Refactor TemplatesPage to fetch user templates from the server and enhance UI
    - Replaced the hardcoded template data with a call to the `getTemplatesForUser` function for dynamic data retrieval.
    - Improved the layout and styling of the templates page, including a new header and a button for creating new templates.
    - Integrated the `TemplateList` component to display user templates, enhancing the overall user experience and interface consistency.

--- nextjs/app/(dashboard)/templates/page.tsx
+++ nextjs/app/(dashboard)/templates/page.tsx
@@ -1,34 +1,34 @@
+import TemplateList from "@/components/TemplateList";
+import { Button } from "@/components/ui/button";
+import { createTemplate } from "@/server/mutations";
+import { getTemplatesForUser } from "@/server/queries";
+import { Plus } from "lucide-react";
 import React from "react";
 
-type Template = {
-  name: string;
-  description: string;
-};
-
 export default async function TemplatesPage() {
-  const templatesPromise = new Promise<Template[]>((resolve) => {
-    setTimeout(() => {
-      resolve([
-        { name: "template 1", description: "template 1 description" },
-        { name: "template 2", description: "template 2 description" },
-        { name: "template 3", description: "template 3 description" },
-        { name: "template 4", description: "template 4 description" },
-        { name: "template 5", description: "template 5 description" },
-        { name: "template 6", description: "template 6 description" },
-        { name: "template 7", description: "template 7 description" },
-        { name: "template 8", description: "template 8 description" },
-        { name: "template 9", description: "template 9 description" },
-        { name: "template 10", description: "template 10 description" },
-      ]);
-    }, 3000);
-  });
-  const templates = await templatesPromise;
+  const templates = await getTemplatesForUser();
 
   return (
-    <div>
-      {templates.map((template) => (
-        <div key={template.name}>{template.name}</div>
-      ))}
+    <div className="w-full">
+      <div className="max-w-screen-2xl mx-auto p-4 sm:p-6 md:p-8 lg:p-12 mt-2 space-y-6 sm:space-y-8 lg:space-y-10">
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mb-4 sm:mb-6">
+          <div className="space-y-2 sm:space-y-4 mb-4 sm:mb-0">
+            <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold">
+              My Templates
+            </h1>
+            <p className="text-sm sm:text-base text-gray-500">
+              View and manage your templates and prompts here
+            </p>
+          </div>
+          <form action={createTemplate} className="w-full sm:w-auto">
+            <Button className="rounded-3xl text-base w-full sm:w-auto">
+              <Plus className="w-4 h-4 mr-1" strokeWidth={3} />
+              New Template
+            </Button>
+          </form>
+        </div>
+        <TemplateList templates={templates} />
+      </div>
     </div>
   );
 }
